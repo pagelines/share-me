7f3232e6bdfabb4881d8f51ecba05a2162d064fa
Jenny <jenny@simplemama.com>
2013-11-14 14:23:59 -0500

    minor description change (no v increment)

--- share-me.php
+++ share-me.php
@@ -3,7 +3,7 @@
 	Plugin Name: Share Me
 	Author: Simple Mama
 	Author URI: http://www.simplemama.com
-	Description: Share Me allows your visitors to share content to the most popular social media sites and includes a customizable instructional greeting.
+	Description: Share Me allows your visitors to share content to the most popular social media sites and includes a customizable instructional greeting. For PageLines DMS only.
 	Class Name: ShareMe
 	Demo:
 	Version: 1.2
